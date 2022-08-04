b72ffed53d1dcdf70c1d264e917fc466ffff2d95
Sviatoslav Starokozhev <slaventiysvat@gmail.com>
2022-08-04 11:48:20 +0300

    added comments

--- dns_my.py
+++ dns_my.py
@@ -1,5 +1,13 @@
 import socket
+#work with my own dns server 
+#guide taked from https://www.youtube.com/watch?v=sjNaoJ_-cvc&ab_channel=howCode
+#from https://www.ietf.org/rfc/rfc1035.txt
+#Network Working Group                                     P. Mockapetris
+#Request for Comments: 1035                                           ISI
+                                                           #November 1987
+#Obsoletes: RFCs 882, 883, 973
 
+#            DOMAIN NAMES - IMPLEMENTATION AND SPECIFICATION
 port = 53;
 ip = "127.0.0.1";
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
