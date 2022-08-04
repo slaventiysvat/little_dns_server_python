38bba2361bd3a0f4543dfe8935d2d6249703a9f1
Sviatoslav Starokozhev <slaventiysvat@gmail.com>
2022-08-04 22:01:09 +0300

    added some changes

--- dns_my.py
+++ dns_my.py
@@ -1,4 +1,5 @@
 import socket
+from sys import byteorder
 import time
 
 #work with my own dns server 
@@ -20,11 +21,33 @@ t = time.monotonic();
 print("it start works", time.monotonic());
 
 
+def getflags(flags):
+    byte1 = bytes(flags[0:1]);
+    byte2 = bytes(flags[1:2]);
+    rflags = "";
+    QR = "1";
+    OPCODE = "";
+    for bit in range(1,5):
+        OPCODE = OPCODE + str(ord(byte1)&(1<<bit)); 
+    AA = "";
+    TC = "0"; #Truncation
+    RD = ""; 
+    RA = "0";
+    Z = "000";
+    RCODE = "0000";
+    return int((QR+OPCODE+AA+TC+RD),2).to_bytes(1,byteorder = "big") + int(RA+Z+RCODE,2).to_bytes(1,byteorder = "big");
 
 def buidresponse(data):
+    #Get Transaction ID
     TransactionId = data[0:2];
+    TID = "";
     for byte in TransactionId:
-        print("byte = ",byte);
+        TID = TID + hex(byte)[2:];
+        #print("byte = ",byte);
+
+    #Get Flags
+    Flags = getflags(data[2:4]);
+    print(Flags);
     return TransactionId;
     
 
